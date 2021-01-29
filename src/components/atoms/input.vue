1529ef62ee337084546dcf05ba1088a6edc0a563
Arman Israelyan <armani.developer@gmail.com>
2021-01-29 19:51:27 +0400

    feature/ add input global style mixins, add input moluecul universal element,

---
+++ src/components/atoms/input.vue
@@ -0,0 +1,45 @@
+<template>
+  <b-form-input :class="inputClassArray" :placeholder="inputPlaceholder" :autocomplete="cutomAutoComplete" :readonly="customReadonly" :autofocus="customAutofocus" />
+</template>
+
+<script>
+export default {
+  name: "custom-input",
+  props: {
+    inputPlaceholder: {
+      type: String,
+      default: null
+    },
+    inputClassArray: {
+      type: Array,
+      default: () => {
+        return ["primary-input"];
+      }
+    },
+    cutomAutoComplete: {
+      type: String,
+      default: 'off'
+    },
+    customReadonly: {
+      type: Boolean,
+      default: false
+    },
+    customAutofocus: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.primary-input {
+  @include inputStyle($google-border, 34px);
+  @include setBackgroundRgba($transparent, 0);
+   &:focus {
+     box-shadow: $none;
+     @include setBackgroundRgba($transparent, 0);
+     @include inputStyle($google-border, 34px);
+   }
+}
+</style>
